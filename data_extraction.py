d5ce296eb809e56a5930453362611c2b5082be5b
aleksandra0014 <a.walczybok4@gmail.com>
2024-10-14 14:02:21 +0200

    new version

--- data_extraction.py
+++ data_extraction.py
@@ -16,49 +16,51 @@ client_credentials_manager = SpotifyClientCredentials(client_id=client_id,
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 # pobieranie piosenek 
-recommendations = sp.recommendations(seed_genres=['pop'], limit=100)
+recommendations1= sp.recommendations(seed_genres=['pop'], limit=100)
+recommendations2 = sp.recommendations(seed_genres=['hip-hop'], limit=100)
+recommendations3 = sp.recommendations(seed_genres=['rock'], limit=100)
 
-# Zbieranie unikalnych albumów
-album_ids = [track['album']['id'] for track in recommendations['tracks']]
 
-'''def artists_from_album(album_id, dict):
-    album_info = sp.album(album_id)
+# zbieranie unikalnych albumów
 
-    # Pobieranie utworów z albumu
-    tracks = sp.album_tracks(album_id)
-    all_artists = set()
-    for track in tracks['items']:
-        for artist in track['artists']:
-            all_artists.add(artist['name'])
-
-    dict[album_info['name']] = all_artists'''
+def get_album_id():
+    album_ids = set()
+    for rec in [recommendations1, recommendations2, recommendations3]:
+        album_ids.update({track['album']['id'] for track in rec['tracks']})
+    return album_ids
 
 def artists_from_album(album_id, artist_album_dict):
     album_info = sp.album(album_id)
 
-    # Pobieranie utworów z albumu
     tracks = sp.album_tracks(album_id)
     
     for track in tracks['items']:
         for artist in track['artists']:
             artist_name = artist['name']
-            # Dodaj album do listy albumów dla danego artysty
             if artist_name not in artist_album_dict:
-                artist_album_dict[artist_name] = set()  # Inicjalizuj listę, jeśli artysta nie istnieje
+                artist_album_dict[artist_name] = set() 
             artist_album_dict[artist_name].add(album_info['name'])
 
 
-artist_album_dict = {}
-for album_id in album_ids:
-    artists_from_album(album_id=album_id, artist_album_dict=artist_album_dict)
+def create_file(album_ids):
+    artist_album_dict = {}
+    for album_id in album_ids:
+        artists_from_album(album_id=album_id, artist_album_dict=artist_album_dict)
 
+    album_pairs = set()
+    for artist, albums in artist_album_dict.items():
+        if len(albums) > 1:
+            pairs = combinations(albums, 2)
+            album_pairs.update(pairs)
 
-album_pairs = set()
-for artist, albums in artist_album_dict.items():
-    if len(albums) > 1:  # Sprawdź, czy artysta występuje na więcej niż jednym albumie
-        pairs = combinations(albums, 2)  # Tworzy pary albumów
-        album_pairs.update(pairs)
+    with open('albums_pairs.txt', 'w', encoding='utf-8') as f:
+        for album1, album2 in album_pairs:
+            f.write(f"{album1}, {album2}\n")
 
-with open('albums.txt', 'w', encoding='utf-8') as f:
-    for album1, album2 in album_pairs:
-        f.write(f"{album1}, {album2}\n")
+    with open('all_albums.txt', 'w', encoding='utf-8') as f:
+        for id in album_ids:
+            f.write(f"{sp.album(album_id=id)['name']}\n")
+
+if __name__ == '__main__':
+    album_ids = get_album_id()
+    create_file(album_ids=album_ids)
